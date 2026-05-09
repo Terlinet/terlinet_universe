ca60e72cfab3688be8475aba02809455c9e624ca
Tertuliano Barbosa <arazaodetudoeoamor@gmail.com>
2026-05-09 13:07:00 -0300

    Deploy: Robust IA stabilization update

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"59aa584fdf100e6c78c785d8a5b565d1de4b48
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "494732923" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "288931328" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
