8667f5073c3e5d0a35bbb855f4d209c73579d0ad
Tertuliano Barbosa <arazaodetudoeoamor@gmail.com>
2026-05-10 13:28:58 -0300

    Deploy: Hologram factory compilation fix

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"59aa584fdf100e6c78c785d8a5b565d1de4b48
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2457276344" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1886746146" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
